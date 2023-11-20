442266dfe06ff64229ba7c50b8038f82b8f909fb
Igor Sprovieri <igsp1@hotmail.com>
2023-11-20 13:08:50 -0300

    Feat: Bcrypt password

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,13 +2,19 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto';
 import { User } from './user.entity';
 import { PrismaService } from 'src/db/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
-    return await this.prisma.user.create({ data: createUserDto });
+    return await this.prisma.user.create({
+      data: {
+        ...createUserDto,
+        password: await bcrypt.hash(createUserDto.email, 10),
+      },
+    });
   }
 
   async findByEmail(email: string): Promise<User> {
